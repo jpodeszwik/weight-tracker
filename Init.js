260d075c6bad27f7ac759c39eadbbcee5a4c3976
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2015-12-11 23:44:41 +0100

    Changing recent user data on select

--- Init.js
+++ Init.js
@@ -1,14 +1,23 @@
 $(function () {
     var es = new Elasticsearch('http://zbiki.ddns.net');
-    es.listUsers(function (users) {
-        users.forEach(function (user) {
-            $('.user-selector').append($('<option>', {value: user}).text(user))
-        });
 
+    function updateRecentData() {
         es.recentUserData($('.user-selector').val(), function (userData) {
+            $('.recent-data').empty();
             userData.forEach(function (data) {
                 $('.recent-data').append($('<tr><td>' + data.date + '</td><td>' + data.weight + '</td></tr>'))
             });
         });
+    }
+
+    es.listUsers(function (users) {
+        users.forEach(function (user) {
+            $('.user-selector').append($('<option>', {value: user}).text(user))
+        });
+        updateRecentData();
+    });
+
+    $('.user-selector').change(function() {
+        updateRecentData();
     });
 });
