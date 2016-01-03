ac5b636503dd5821d5f4f54799f698e7e2dcf0c2
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2016-01-03 14:07:58 +0100

    Every user as separate type in weight index

--- Elasticsearch.js
+++ Elasticsearch.js
@@ -1,41 +1,28 @@
-var usernameAggregation = '\
-    {\
-        "size": 0,\
-        "aggregations" : {\
-            "users" : {\
-                "terms" : {\
-                    "field" : "username",\
-                    "size" : "1000"\
-                }\
-            }\
-        }\
-    }';
-
-function recentUserDataQuery(username) {
-    return '\
-       {\
-           "query": {\
-               "term" : {\
-                   "username" : "' + username + '"\
-                }\
-            },\
-            "sort" : [\
-                {\
-                    "@timestamp": {\
-                        "order" : "desc"\
-                    }\
-                }\
-            ]\
-        }';
-}
+var allRecordsOrderedByTimestampQuery = '\
+   {\
+       "query": {\
+           "match_all" : {}\
+       },\
+       "sort" : [\
+           {\
+               "@timestamp": {\
+                   "order" : "desc"\
+               }\
+           }\
+       ]\
+   }';
 
 function Elasticsearch(esUrl) {
     this.esUrl = esUrl;
 
     this.listUsers = function (onSuccess) {
-        $.post(this.esUrl + '/weight/_search', usernameAggregation, function (data) {
-            var users = data["aggregations"]["users"]["buckets"].map(function (bucket) {
-                return bucket["key"];
+        $.get(this.esUrl + '/weight/_mappings', function (data) {
+            var users = Object.keys(data).map(function (index) {
+                return Object.keys(data[index]['mappings']);
+            }).reduce(function (a, b) {
+                return a.concat(b);
+            }, []).filter(function (type) {
+                return type != '_default_'
             });
 
             onSuccess(users);
@@ -43,7 +30,7 @@ function Elasticsearch(esUrl) {
     };
 
     this.recentUserData = function (user, onSuccess) {
-        $.post(this.esUrl + '/weight/_search', recentUserDataQuery(user), function (data) {
+        $.post(this.esUrl + '/weight/' + user + '/_search', allRecordsOrderedByTimestampQuery, function (data) {
             var userData = data['hits']['hits'].map(function (hit) {
                 var source = hit['_source'];
                 return {
