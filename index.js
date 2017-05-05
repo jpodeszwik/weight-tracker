fd34007c926a85a11b24b5a595bf978b2512e0fe
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2017-05-05 23:17:42 +0200

    Added possibility to specify callback host via environment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: `http://${process.env.CB_HOST}/auth/google/callback`
   },
   function(accessToken, refreshToken, profile, done) {
     return done(null, {id: profile.id, name: profile.displayName, email: profile.emails[0].value});
