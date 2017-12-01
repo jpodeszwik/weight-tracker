32715fc04d198a0572836003edc8da4c488858bf
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2017-12-02 00:56:06 +0100

    using tokens sent by frontend

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ var espressSession = require('express-session');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var CustomStrategy = require('passport-custom')
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.GOOGLE_CALLBACK_HOST}/auth/google/callback`
-  },
-  function(accessToken, refreshToken, profile, done) {
-    return done(null, {id: profile.id, name: profile.displayName, email: profile.emails[0].value});
-  }
+var GoogleAuth = require('google-auth-library');
+
+var auth = new GoogleAuth;
+
+var clientId = process.env.CLIENT_ID;
+
+var client = new auth.OAuth2(clientId);
+
+passport.use(new CustomStrategy(
+  function(req, done) {
+    client.verifyIdToken(
+        req.body.token,
+        clientId,
+        function(e, login) {
+            if(e) {
+                console.warn(e);
+                return done(null, false);
+            }
+
+            var payload = login.getPayload();
+            var userId = payload['sub'];
+            console.log(userId);
+
+            done(null, {'id': userId});
+        }
+    )}
 ));
 
 passport.serializeUser(function(user, done) {
@@ -25,6 +43,7 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
+
 var app = express();
 
 app.use(cookieParser());
@@ -34,25 +53,23 @@ app.use(passport.session());
 app.use(bodyParser.json());
 
 
-app.use('/api', api);
+app.post('/login',
+  passport.authenticate('custom'),
+    function(req, res) {
+      if(req.isAuthenticated()) res.status(200).send('login success');
+      else res.status(401).send('unathorized');
+    }
+);
+
+//app.use('/api', api);
 
 app.get('/',function(req,res){
   if (!req.isAuthenticated()) {
-    res.redirect('/auth/google');
+    res.send(401).send('unauthorized');
   } else {
-    console.log(req.user);
     res.sendFile(__dirname + '/static/index.html');
   }
 });
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email profile'] }));
-
-
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/');
-  });
 app.use(express.static('static'))
 app.listen(3000);
